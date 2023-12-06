76d4110a65f7e7f5941bd80ab4704cc8dcd17be1
Jay Convertino <johnathan.convertino.1@us.af.mil>
2023-12-06 10:00:45 -0500

    fix pl gen sythn checkpoint setting and gen command for all zynq 32 bit

--- zc702/system_pl_gen.tcl
+++ zc702/system_pl_gen.tcl
@@ -9,8 +9,14 @@ set_property CONFIG.M01_A00_BASE_ADDR {0x41620000} [get_ips axi_crossbar_pl]
 set_property CONFIG.M00_A00_ADDR_WIDTH {29} [get_ips axi_crossbar_pl]
 set_property CONFIG.M01_A00_ADDR_WIDTH {12} [get_ips axi_crossbar_pl]
 
+set_property generate_synth_checkpoint false [get_files axi_crossbar_pl.xci]
+
+generate_target all [get_ips axi_crossbar_pl]
+
 vivado_ip_vlvn_version_check "xilinx.com:ip:axi_iic:2.1"
 
 create_ip -vlnv xilinx.com:ip:axi_iic:2.1 -module_name axi_iic_fmc
 
 set_property generate_synth_checkpoint false [get_files axi_iic_fmc.xci]
+
+generate_target all [get_ips axi_iic_fmc]
